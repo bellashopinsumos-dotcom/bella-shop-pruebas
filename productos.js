d1c528119c14af06ca62e4670e6539c504d90792
bellashopinsumos-dotcom <bellashopinsumos@gmail.com>
2026-09-10 15:18:46 -0500

    Update productos.js

--- productos.js
+++ productos.js
@@ -1,10 +1,18 @@
 const productos = [
   {
+    id: "gel-saturacion-queenigth",
     nombre: "Gel de Saturación Queenigth",
     precio: 15000,
     categoria: "Nail Art",
     subcategoria: "Gel de Saturación",
     marca: "Queenigth",
-    imagen: "imagenes/Captura de pantalla 2026-09-10 144809.png"
+    imagen: "imagenes/Captura de pantalla 2026-09-10 144809.png",
+
+    referencias: [
+      "Rose Gold",
+      "Champagne Gold",
+      "Gold",
+      "Silver"
+    ]
   }
 ];
